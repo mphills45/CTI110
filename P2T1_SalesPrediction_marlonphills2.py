49680984464c1b76bfd0b203fa6559e162f39264
mphills45 <43525769+mphills45@users.noreply.github.com>
2018-09-23 18:40:47 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_marlonphills2.py
@@ -0,0 +1,8 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\mphills45\iCloudDrive\CTI110\P2T1_SalesPrediction_marlonphills.py 
+enter your Name: Marlon Phills
+Enter the projected sales: 200000
+ The profit is $ 46,000.00
+>>> 
