982bf80557e6e7062031bebb026fce7d18d2cfd1
mphills45 <43525769+mphills45@users.noreply.github.com>
2018-09-23 20:26:25 -0400

    Add files via upload
    First submission

--- M1T1_phills.py
+++ M1T1_phills.py
@@ -2,7 +2,10 @@ Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Inte
 Type "copyright", "credits" or "license()" for more information.
 >>> 
  RESTART: C:\Users\mphills45\iCloudDrive\CTI110\P2T1_SalesPrediction_marlonphills.py 
-enter your Name: Marlon Phills
+Tutoral projected total sales
+14 June 2018
+CTI-110 P2T1 - projected total sales
+enter your Name: marlon phills
 Enter the projected sales: 200000
  The profit is $ 46,000.00
 >>> 
